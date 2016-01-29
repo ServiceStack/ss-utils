2bb6a5ebdcb899b2856e5c6b62b3008df3beed13
Demis Bellot <demis.bellot@gmail.com>
2016-01-29 09:09:09 -0500

    convert to umd

--- ss-utils.js
+++ ss-utils.js
@@ -1,4 +1,29 @@
-(function ($) {
+// Based off https://github.com/ForbesLindesay/umd/blob/master/template.js
+;(function(f) {
+  // CommonJS
+  if (typeof exports === "object" && typeof module !== "undefined") {
+    module.exports = f(require('jquery'));
+  // RequireJS
+  } else if (typeof define === "function" && define.amd) {
+    define(['jquery'], f);
+  // <script>
+  } else {
+    var g
+    if (typeof window !== "undefined") {
+      g = window;
+    } else if (typeof global !== "undefined") {
+      g = global;
+    } else if (typeof self !== "undefined") {
+      g = self;
+    } else {
+      // works providing we're not in "use strict";
+      // needed for Java 8 Nashorn
+      // see https://github.com/facebook/react/issues/3037
+      g = this;
+    }
+    g.jQuery = f(g.jQuery);
+  }
+})(function ($) {
 
     if (!$.ss) $.ss = {};
     $.ss.handlers = {};
@@ -454,5 +479,4 @@
         }
         $.ss.eventSource.onmessage = onMessage;
     };
-
-})(jQuery);
+});
